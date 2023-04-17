fe92556a2b273e42f9a97be33860a5d747289a09
perihanimamoglu <perihanimamoglu1@gmail.com>
2023-04-17 09:20:51 +0300

    implementation of navigation component

--- app/src/main/java/com/perihanimamoglu/satellites/ui/main/MainActivity.kt
+++ app/src/main/java/com/perihanimamoglu/satellites/ui/main/MainActivity.kt
@@ -2,24 +2,14 @@ package com.perihanimamoglu.satellites.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
 import com.perihanimamoglu.satellites.R
-import com.perihanimamoglu.satellites.databinding.ActivityMainBinding
-import com.perihanimamoglu.satellites.ui.satellites.SatelliteFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        supportFragmentManager.commit {
-            add<SatelliteFragment>(R.id.frameLayout)
-        }
+        setContentView(R.layout.activity_main)
     }
 }

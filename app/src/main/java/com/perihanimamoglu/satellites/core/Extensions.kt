f27227128dab7f693a6d6217caa39887b261a6aa
perihanimamoglu <perihanimamoglu1@gmail.com>
2023-04-17 09:22:21 +0300

    add extensions class

--- app/src/main/java/com/perihanimamoglu/satellites/core/Extensions.kt
+++ app/src/main/java/com/perihanimamoglu/satellites/core/Extensions.kt
@@ -1,6 +1,13 @@
 package com.perihanimamoglu.satellites.core
 
+import android.annotation.SuppressLint
 import android.content.res.AssetManager
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun AssetManager.readFile(fileName: String) =
     this.open(fileName).bufferedReader().use { it.readText() }
@@ -10,3 +17,17 @@ fun Int?.orZero() = this ?: 0
 fun Boolean?.orFalse() = this ?: false
 
 fun Long?.orZero() = this ?: 0L
+
+fun Double?.orZero() = this ?: 0.0
+
+@SuppressLint("SimpleDateFormat")
+fun String.convertToDate(): Date? {
+    val dateFormat = SimpleDateFormat("yyyy-mm-dd")
+    return dateFormat.parse(this)
+}
+
+@SuppressLint("SimpleDateFormat")
+fun Date.convertToString(): String {
+    val dateFormat = SimpleDateFormat("dd.mm.yyyy")
+    return dateFormat.format(this)
+}
